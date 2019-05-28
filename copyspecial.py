0f255db93571ab0cd215f05c963200b266857bbd
Tyler <tymitchell2100@gmail.com>
2019-05-28 14:07:47 -0400

    finished

--- copyspecial.py
+++ copyspecial.py
@@ -11,35 +11,64 @@
 
 import re
 import os
+from os import listdir
+from os.path import isfile, join
 import shutil
 import subprocess
 import argparse
 
 # This is to help coaches and graders identify student assignments
-__author__ = "???"
-
+__author__ = "Tyler Ward"
 
 # +++your code here+++
 # Write functions and modify main() to call them
+def find_special_files(my_path):
+    files = [f for f in listdir(my_path) if isfile(join(my_path, f))]
+    special_files = []
+    for filename in files:
+        if re.search('__(.[A-Za-z]*)__', filename):
+            special_files.append(filename)
+    return special_files
+
+def print_filenames(filenames, my_path):
+    for file in filenames:
+        print(my_path)
+        print(file)
+
+def copy_to_dir(filenames, my_path):
+    for file in filenames:
+        shutil.copy(file, my_path)
 
 def main():
     # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
+    parser.add_argument('directory')
     # TODO need an argument to pick up 'from_dir'
     args = parser.parse_args()
+    print(args.directory)
 
-    # TODO you must write your own code to get the cmdline args.
-    # Read the docs and examples for the argparse module about how to do this.
-
-    # Parsing command line arguments is a must-have skill.
-    # This is input data validation.  If something is wrong (or missing) with any
-    # required args, the general rule is to print a usage message and exit(1).
+    if not args.directory:
+        print('Must enter a directory to search')
+        return
 
     # +++your code here+++
     # Call your functions
+    if args.directory == '.':
+        my_path = os.getcwd()
+        special_files = find_special_files(my_path)
+    else:
+        my_path = os.getcwd() + '/' + args.directory
+        special_files = find_special_files(my_path)
+    
+    if args.todir:
+        copy_to_dir(special_files, args.todir)
+    
+    if args.tozip:
+        special_files_str = " ".join(special_files)
+        subprocess.call('zip -j ' + args.tozip + ' ' + special_files_str, shell=True)
 
-
+    print_filenames(special_files, my_path)
 if __name__ == "__main__":
     main()
